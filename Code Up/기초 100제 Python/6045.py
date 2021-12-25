95b66183b8108b0698f265426548f58a1992f963
hs96wings <hs96wings@gmail.com>
2021-12-25 17:54:03 +0900

    211225 17:55

---
+++ Code Up/기초 100제 Python/6045.py
@@ -0,0 +1,5 @@
+# 6045 : [기초-산술연산] 정수 3개 입력받아 합과 평균 출력하기(설명)(py)
+a, b, c = map(int, input().split())
+d = a + b + c
+e = d / 3
+print(d, format(e, '.2f'))
---
+++ Code Up/기초 100제 Python/6053.py
@@ -0,0 +1,3 @@
+# 6053 : [기초-논리연산] 참 거짓 바꾸기(설명)(py)
+n = bool(int(input()))
+print(not n)
