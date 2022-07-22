a6d4ce41ecbca034826e2e386759fbaf143b70da
hs96wings <hs96wings@gmail.com>
2022-07-22 23:22:28 +0900

    220722

--- 분류 대기/2864.py
+++
@@ -1,9 +0,0 @@
-# 2864번 5와 6의 차이 (그리디 알고리즘)
-
-import sys
-
-a, b = map(str, sys.stdin.readline().rstrip().split())
-min = int(a.replace('6', '5')) + int(b.replace('6', '5'))
-max = int(a.replace('5', '6')) + int(b.replace('5', '6'))
-
-print(min, max)
